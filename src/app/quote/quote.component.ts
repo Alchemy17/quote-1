ed339b8d5946e13b9496d0a12331da8b726efc3f
olamijinadebayo <consultolamijin@gmail.com>
2018-03-20 05:50:24 +0300

    this will add styling to the page

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote(1, 'Adebayo Ola', 'Donald Trump', 'Failure is not an option', new Date(2018, 3, 14)),
-    new Quote(2, 'Adebayo Ola', 'Donald Trump', 'Failure is not an option', new Date(2018, 4, 14)),
+    new Quote(1, 'Adebayo Ola', 'Albert Einstein', 'The difference between stupidity and genius is that genius has its limits', new Date(2018, 3, 14)),
+    new Quote(2, 'Adebayo Ola', 'Winston Churchill', "A good speech should be like a woman's skirt, long enough to cover the subject and short enough to create interest", new Date(2018, 4, 14)),
     new Quote(3, 'Adebayo Ola', 'Donald Trump', 'Failure is not an option', new Date(2020, 3, 14)),
   ];
+
 
   addNewQuote(quote) {
     const quoteLength = this.quotes.length;
